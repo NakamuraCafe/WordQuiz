3220e1a3c88f942c04d5e63e30a3b451a307b590
Nakamura <nakamu130301@gmail.com>
2024-12-18 23:04:34 +0900

    ユニットテストを実装

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.core.management import call_command
+from io import StringIO
+from unittest.mock import patch
 
-# Create your tests here.
+class CommandTestCase(TestCase):
+
+    def test_game_clear(self):
+        food = "apple"
+        user_inputs = list(food)
+        with patch('random.choice', return_value=food), patch('getpass.getpass', side_effect=user_inputs):
+            out = StringIO()
+            with patch('sys.stdout', out):
+                call_command('word_quiz')
+                output = out.getvalue()
+
+            self.assertIn(f'ゲームクリア! 答え: {food}', output)
+            self.assertNotIn(f'ゲームオーバー 答え: {food}', output)
+
+    def test_game_over(self):
+        food = "apple"
+        user_inputs = ["x", "y", "z", "u", "v"]
+        with patch('random.choice', return_value=food), patch('getpass.getpass', side_effect=user_inputs):
+            out = StringIO()
+            with patch('sys.stdout', out):
+                call_command('word_quiz')
+                output = out.getvalue()
+
+            self.assertIn('ゲームオーバー 答え: apple', output)
+            self.assertNotIn('ゲームクリア! 答え: apple', output)
